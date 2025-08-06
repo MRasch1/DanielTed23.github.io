56d6508359b5920e2b7862080ab71be85abe5461
Magnus R. <97102126+MRasch1@users.noreply.github.com>
2025-08-06 12:10:23 +0200

    Update001

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/DanielTed23.github.io/',
+  baseHref: './',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/DanielTed23.github.io"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 892, hash: '07ebec0e886528f39431de0f901877b48c91b7353e2211ffd5eb59038714d481', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: '51ef672147adf7df4329c9c1068703070950c45db8ecb92e8655c86975690f2a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 3780, hash: '88f013368a60fb182db0f3f896412634c57c9fad5e294b6bbcaec0e0c0173151', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 871, hash: '9770ed6d55cfd8f94784e08dbfb8164c27f986a474171d92f45a6de353786f62', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1006, hash: 'b45330c0520b4f7c3f8d10933a468f285d0646158d4ed5819d85357fd9a964d5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 3782, hash: 'e4572beb3c7307c4c62204c15eea12c5cb410d050e0c1da303e8b388bda8bf00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-NT7XA6KR.css': {size: 253, hash: 'f/vkHxiydNw', text: () => import('./assets-chunks/styles-NT7XA6KR_css.mjs').then(m => m.default)}
   },
 };
